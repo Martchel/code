f48717bad8b16104a8425750860f9a6cdfbd660a
Marcello Vincent <marcello.vincent@ansys.com>
2023-08-04 18:50:58 +0200

    first commit

---
+++ exercice_1.py
@@ -0,0 +1,36 @@
+class ToolBox:
+
+    def __init__(self):
+        self.tools = []
+
+    def add_tool(self, tools):
+        pass
+
+    def remove_tool(self, tools):
+        pass
+
+
+class Screw:
+
+    def __int__(self,size):
+        self.size = size
+
+    def tighten(self, screw):
+        pass
+
+    def loosen(self, screw):
+        pass
+
+class Hammer:
+
+    def __init__(self, color="red"):
+        self.color = color
+
+    def paint(self, color):
+        pass
+
+    def hammer_in(self, nail):
+        pass
+
+    def remove(self, nail):
+        pass
