829056ce76c7508ce6f1e750504b7edb7ffac165
Marcello Vincent <marcello.vincent@ansys.com>
2023-08-18 14:35:20 +0200

    update

--- film/main.py
+++ film/main.py
@@ -1,14 +1,10 @@
 from Bibliothèque.movie import Film
 
-from Bibliothèque.dvd import FilmDVD
-from Bibliothèque.vhf import FilmVHF
-
 
 def main():
     """Programme Principal.
     """
-alice = Film("Alice", FilmDVD("DVD"))
-bob = Film("Bob", FilmVHF("VHF"))
+
 
 
 if __name__ == "__main__":
